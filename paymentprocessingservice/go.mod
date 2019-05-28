dba1233a44c505941cd1ce1b77dc689f37fdfa1c
Xavier Basty <xbasty@gmail.com>
2019-05-28 11:10:11 +0200

    Add command line handlers for payment processing and query services

--- paymentprocessingservice/go.mod
+++ paymentprocessingservice/go.mod
@@ -1,3 +1,15 @@
 module github.com/xav/f3/paymentprocessingservice
 
 go 1.12
+
+require (
+	github.com/apex/log v1.1.0
+	github.com/fatih/color v1.7.0 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/nats-io/go-nats v1.7.2 // indirect
+	github.com/nats-io/nats.go v1.7.2
+	github.com/nats-io/nkeys v0.0.2 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/spf13/cobra v0.0.4
+)
---
+++ paymentqueryservice/command/start.go
@@ -0,0 +1,53 @@
+// Copyright © 2019 Xavier Basty <xbasty@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package command
+
+import (
+	"github.com/apex/log"
+	"github.com/nats-io/nats.go"
+	"github.com/spf13/cobra"
+)
+
+type Server struct {
+}
+
+var (
+	natsAddr string
+	nc       *nats.Conn
+)
+
+// Init returns the runnable cobra command.
+func (c *Server) Init() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "start",
+		Short: "Start the payment query service",
+		Run:   c.startServer,
+	}
+
+	cmd.PersistentFlags().StringVarP(&natsAddr, "nats-addr", "n", nats.DefaultURL, "URL of the NATS server.")
+
+	return cmd
+}
+
+func (c *Server) startServer(cmd *cobra.Command, args []string) {
+	log.Infof("starting payment query service")
+
+	// Connect to the NATS server
+	var err error
+	nc, err = nats.Connect(natsAddr)
+	if err != nil {
+		log.WithError(err).Fatalf("failed to connect to NATS server '%v'", natsAddr)
+	}
+}
